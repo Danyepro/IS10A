991b08b3d9e20f54ab7f8f195ce93c962d87dff6
carlosdaneryr <carlosdaneryr0228@dev.azure.com>
2024-12-07 19:53:50 -0600

    add all

--- Proyecto_IS - final/src/ConsoleApp/Program.cs
+++ Proyecto_IS - final/src/ConsoleApp/Program.cs
@@ -1,18 +1,32 @@
-﻿double ladoA = 5; // Corresponde a 'a' (un lado del triángulo)
-double ladoB = 3; // Corresponde a 'b' (la base del triángulo)
-double ladoC = 4; // Corresponde a 'c' (otro lado del triángulo)
-double alturaTriangulo = 4; // Altura del triángulo correspondiente a la base 'b'
-double alturaPrisma = 10; // Altura del prisma triangular
+﻿using Application;
 
-// Cálculo del perímetro del triángulo
-double perimetro = Application.Triangulo.Perimetro(ladoA, ladoB, ladoC);
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Lados del triángulo base
+        double ladoA = 5.0; // Primer lado del triángulo
+        double ladoB = 6.0; // Base del triángulo
+        double ladoC = 7.0; // Tercer lado del triángulo
 
-// Cálculo del área de la base triangular
-double area = Application.Triangulo.Area(ladoB, alturaTriangulo);
+        // Altura del triángulo correspondiente a la base 'ladoB'
+        double alturaTriangulo = 4.0;
 
-// Cálculo del área por altura
-double areaPorAltura = area * alturaPrisma;
+        // Altura del prisma
+        double alturaPrisma = 10.0;
 
-Console.WriteLine($"El perímetro de la base de un triángulo con lados de {ladoA}cm, {ladoB}cm y {ladoC}cm es {perimetro}cm");
-Console.WriteLine($"El área de la base de un triángulo con base de {ladoB}cm y altura de {alturaTriangulo}cm es {area}cm2");
-Console.WriteLine($"El resultado de multiplicar el área de la base por la altura del prisma es {areaPorAltura}cm3");
+        // Cálculo del perímetro del triángulo
+        double perimetro = Triangulo.Perimetro(ladoA, ladoB, ladoC);
+
+        // Cálculo del área de la base triangular
+        double area = Triangulo.Area(ladoB, alturaTriangulo);
+
+        // Cálculo del volumen del prisma triangular
+        double volumen = PrismaTriangular.Volumen(area, alturaPrisma);
+
+        // Salida de los resultados
+        Console.WriteLine($"El perímetro de la base de un Prisma Triangular con lados de {ladoA}cm, {ladoB}cm y {ladoC}cm es {perimetro}cm");
+        Console.WriteLine($"El área de la base de un Prisma Triangular con base de {ladoB}cm y altura de {alturaTriangulo}cm es {area}cm²");
+        Console.WriteLine($"El volumen de un Prisma Triangular con área de base de {area}cm² y altura de {alturaPrisma}cm es {volumen}cm³");
+    }
+}
